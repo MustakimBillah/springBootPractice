523fc51f050cbeac15543ed082f43f19f11f7d9d
Mustakim Billah <mustakim.billah@revesoft.com>
2022-06-17 21:01:35 +0600

    slf4j logger testing

--- src/main/java/com/mustakim/springBoot/practice/controller/DepartmentController.java
+++ src/main/java/com/mustakim/springBoot/practice/controller/DepartmentController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,13 +24,17 @@ public class DepartmentController {
 	@Autowired
 	private DepartmentService departmentService;
 	
+	private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+	
 	@PostMapping("/saveDepartment")
 	public Department saveDepartment(@Valid @RequestBody Department department) {
+		LOGGER.info("Inside saveDepartment of DepartmentController");
 		return departmentService.saveDepartment(department);
 	}
 	
 	@GetMapping("/departments")
 	public List<Department> fetchDepartments(){
+		LOGGER.info("Inside fetchDepartments of DepartmentController");
 		return departmentService.fetchDepartments();
 	}
 	
