3cc1d7c733efdd0ed33a79b8684c44567cd1a242
Mustakim Billah <mustakim.billah@revesoft.com>
2022-06-17 21:46:12 +0600

    introduced lombok to remove boiler plate codes

--- src/main/java/com/mustakim/springBoot/practice/entity/Department.java
+++ src/main/java/com/mustakim/springBoot/practice/entity/Department.java
@@ -8,9 +8,15 @@ import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
 
-@Entity
-public class Department {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long departmentID;
@@ -19,46 +25,4 @@ public class Department {
 	private String departmentName;
 	private String departmentCode;
 	private String departmentAddress;
-	
-	public Long getDepartmentID() {
-		return departmentID;
-	}
-	public void setDepartmentID(Long departmentID) {
-		this.departmentID = departmentID;
-	}
-	public String getDepartmentName() {
-		return departmentName;
-	}
-	public void setDepartmentName(String departmentName) {
-		this.departmentName = departmentName;
-	}
-	public String getDepartmentCode() {
-		return departmentCode;
-	}
-	public void setDepartmentCode(String departmentCode) {
-		this.departmentCode = departmentCode;
-	}
-	public String getDepartmentAddress() {
-		return departmentAddress;
-	}
-	public void setDepartmentAddress(String departmentAddress) {
-		this.departmentAddress = departmentAddress;
-	}
-	public Department(Long departmentID, String departmentName, String departmentCode, String departmentAddress) {
-		super();
-		this.departmentID = departmentID;
-		this.departmentName = departmentName;
-		this.departmentCode = departmentCode;
-		this.departmentAddress = departmentAddress;
-	}
-	public Department() {
-
-	}
-	@Override
-	public String toString() {
-		return "Department [departmentID=" + departmentID + ", departmentName=" + departmentName + ", departmentCode="
-				+ departmentCode + ", departmentAddress=" + departmentAddress + "]";
-	}
-	
-	
 }
