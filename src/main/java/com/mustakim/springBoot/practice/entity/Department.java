c33b2d558647d421592747c6b574ac38b3bf7582
Mustakim Billah <mustakim.billah@revesoft.com>
2022-06-17 20:31:43 +0600

    Hibernate Validators

--- src/main/java/com/mustakim/springBoot/practice/entity/Department.java
+++ src/main/java/com/mustakim/springBoot/practice/entity/Department.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 public class Department {
@@ -11,6 +14,8 @@ public class Department {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long departmentID;
+	@NotBlank(message="Please Enter Department Name")
+	@Length(max = 5,min=3)
 	private String departmentName;
 	private String departmentCode;
 	private String departmentAddress;
